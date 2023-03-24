839b6341292903f0405df3a8ac782d650c77e01b
Rui Carneiro <rui.motacarneiro@heig-vd.ch>
2023-03-24 11:12:37 +0100

    ValidSet

--- set.hs
+++ set.hs
@@ -10,6 +10,9 @@ bound = 1000
 
 type Set = Int -> Bool
 
+validSet::Set -> Set
+validSet set x = set x && (x>=(-bound)) && (x<=bound)
+
 instance Eq Set where
     (==) s1 s2 = check (-bound) where
         check x
@@ -36,7 +39,7 @@ instance Show Set where
             | set x && previous = show' (x+1) set True False False
             | set x && not previous = show x ++ show' (x+1) set True True False
 
---Bonus : It works, just be sure to write : subSet `⊆` mainSet
+--Bonus : It works, just be sure to write : subSet ⊆ mainSet, val `ϵ` set
 (⊆) :: Set -> Set -> Bool
 (⊆) sub main = check (-bound) where
     check x
@@ -51,19 +54,19 @@ emptySet::Set
 emptySet x = False
 
 elem' :: Set -> Int -> Bool
-elem' set = set
+elem' = validSet
 
 singleton :: Int -> Set
-singleton x = (==x)
+singleton x = validSet (==x)
 
 union :: Set -> Set -> Set
-union s1 s2 x = s1 x || s2 x
+union s1 s2 x = validSet s1 x || s2 x
 
 intersect :: Set -> Set -> Set
-intersect s1 s2 x = s1 x && s2 x
+intersect s1 s2 x = validSet s1 x && s2 x
 
 diff :: Set -> Set -> Set
-diff main sub x = main x && not(sub x)
+diff main sub x = validSet main x && not(sub x)
 
 filter' :: Set -> (Int -> Bool) -> Set
 filter' = intersect
@@ -75,7 +78,7 @@ any' :: Set -> (Int -> Bool) -> Bool
 any' set predicat = intersect set predicat /= emptySet
 
 map' :: Set -> (Int -> Int) -> Set
-map' set function = check (-bound) emptySet where
+map' set function = validSet (check (-bound) emptySet) where
     check x resultSet
         | x == bound + 1 = resultSet
         | set x = check (x+1) resultSet `union` singleton (function x)
